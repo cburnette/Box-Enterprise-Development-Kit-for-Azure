bff60f66b952bb5390378d7d3567092b3a91d58a
Chad Burnette <chadburnette@me.com>
2018-05-21 16:01:17 -0400

    DRY'd up the code for creating skills cards

--- BoxSkillsHelper/SkillsHelper.cs
+++ BoxSkillsHelper/SkillsHelper.cs
@@ -5,31 +5,125 @@ namespace Box.EnterpriseDevelopmentKit.Azure
 {
     public class SkillsHelper
     {
-        public static JObject CreateTranscriptCard(string title, string serviceName, string skillInvocationId, IList<TranscriptCardEntry> entries, double? duration=null)
+        public static Dictionary<string, object> CreateCardMetadata(JArray cards)
+        {
+            var cardMetadata = new JObject(
+                new JProperty("cards", cards)
+            );
+
+            return cardMetadata.ToObject<Dictionary<string, object>>();
+        }
+
+        public static JObject CreateTranscriptCard(string title, string serviceName, string skillInvocationId, IList<SkillCardEntry> entries, double? duration = null)
+        {
+            return CreateSkillCard("transcript", title, serviceName, skillInvocationId, entries, duration);
+        }
+
+        public static JObject CreateKeywordCard(string title, string serviceName, string skillInvocationId, IList<SkillCardEntry> entries, double? duration = null)
+        {
+            return CreateSkillCard("keyword", title, serviceName, skillInvocationId, entries, duration);
+        }
+
+        public static JObject CreateTimelineCard(string title, string serviceName, string skillInvocationId, IList<SkillCardEntry> entries, double? duration = null)
+        {
+            return CreateSkillCard("timeline", title, serviceName, skillInvocationId, entries, duration);
+        } 
+
+        public class TranscriptCardEntry : SkillCardEntry
+        {
+            public TranscriptCardEntry(string text, IList<EntryAppearance> appearances)
+            {
+                _type = "text";
+                _text = text;
+                _appearances = appearances;
+            }
+        }
+
+        public class KeywordCardEntry : SkillCardEntry
+        {
+            public KeywordCardEntry(string text, IList<EntryAppearance> appearances)
+            {
+                _type = "text";
+                _text = text;
+                _appearances = appearances;
+            }
+        }
+
+        public class TimelineCardEntry : SkillCardEntry
+        {
+            public TimelineCardEntry(string text, string imageUrl, IList<EntryAppearance> appearances)
+            {
+                _type = "image";
+                _text = text;
+                _imageUrl = imageUrl;
+                _appearances = appearances;
+            }
+        }
+
+        public class EntryAppearance
+        {
+            public double Start { get; }
+            public double End { get; }
+
+            public EntryAppearance(double start, double end)
+            {
+                Start = start;
+                End = end;
+            }
+        }
+
+        public abstract class SkillCardEntry
+        {
+            protected string _type;
+            public string Type { get { return _type; } }
+
+            protected string _text;
+            public string Text { get { return _text; } }
+
+            protected string _imageUrl;
+            public string ImageUrl { get { return _imageUrl; } }
+
+            protected IList<EntryAppearance> _appearances;
+            public IList<EntryAppearance> Appearances { get { return _appearances; } }
+
+            public JObject EntryJson { get
+                {
+                    var appearancesJson = new JArray();
+                    foreach (var appearance in Appearances)
+                    {
+                        var appearanceJson = new JObject(
+                            new JProperty("start", appearance.Start),
+                            new JProperty("end", appearance.End));
+
+                        appearancesJson.Add(appearanceJson);
+                    }
+
+                    var newEntry = new JObject(
+                        new JProperty("type", _type),
+                        new JProperty("text", _text),
+                        new JProperty("appears", appearancesJson));
+
+                    if(!string.IsNullOrEmpty(_imageUrl))
+                    {
+                        newEntry.Add("image_url", _imageUrl);
+                    }
+
+                    return newEntry;
+                }
+            }
+        }
+
+        private static JObject CreateSkillCard(string skillCardType, string title, string serviceName, string skillInvocationId, IList<SkillCardEntry> entries, double? duration = null)
         {
             var entriesJson = new JArray();
             foreach (var entry in entries)
             {
-                var appearances = new JArray();
-                foreach(var appearance in entry.Appearances)
-                {
-                    var appearanceJson = new JObject(
-                        new JProperty("start", appearance.Start),
-                        new JProperty("end", appearance.End));
-
-                    appearances.Add(appearanceJson);
-                }
-
-                var newEntry = new JObject(
-                    new JProperty("text", entry.Text),
-                    new JProperty("appears", appearances));
-
-                entriesJson.Add(newEntry);
+                entriesJson.Add(entry.EntryJson);
             }
 
             JObject transcriptCard = new JObject(
                             new JProperty("type", "skill_card"),
-                            new JProperty("skill_card_type", "transcript"),
+                            new JProperty("skill_card_type", skillCardType),
                             new JProperty("title", title),
                             new JProperty("skill",
                                 new JObject(
@@ -47,146 +141,6 @@ namespace Box.EnterpriseDevelopmentKit.Azure
             }
 
             return transcriptCard;
-        }
-
-        public static JObject CreateKeywordCard(string title, string serviceName, string skillInvocationId, IList<KeywordCardEntry> entries, double? duration = null)
-        {
-            var entriesJson = new JArray();
-            foreach (var entry in entries)
-            {
-                var appearances = new JArray();
-                foreach (var appearance in entry.Appearances)
-                {
-                    var appearanceJson = new JObject(
-                        new JProperty("start", appearance.Start),
-                        new JProperty("end", appearance.End));
-
-                    appearances.Add(appearanceJson);
-                }
-
-                var newEntry = new JObject(
-                    new JProperty("text", entry.Text),
-                    new JProperty("appears", appearances));
-
-                entriesJson.Add(newEntry);
-            }
-
-            JObject keywordCard = new JObject(
-                            new JProperty("type", "skill_card"),
-                            new JProperty("skill_card_type", "keyword"),
-                            new JProperty("title", title),
-                            new JProperty("skill",
-                                new JObject(
-                                    new JProperty("type", "service"),
-                                    new JProperty("id", serviceName))),
-                            new JProperty("invocation",
-                                new JObject(
-                                    new JProperty("type", "skill_invocation"),
-                                    new JProperty("id", skillInvocationId))),
-                            new JProperty("entries", entriesJson));
-
-            if (duration.HasValue)
-            {
-                keywordCard.Add("duration", duration.Value);
-            }
-
-            return keywordCard;
-        }
-
-        public static JObject CreateTimelineCard(string title, string serviceName, string skillInvocationId, IList<TimelineCardEntry> entries, double? duration = null)
-        {
-            var entriesJson = new JArray();
-            foreach (var entry in entries)
-            {
-                var appearances = new JArray();
-                foreach (var appearance in entry.Appearances)
-                {
-                    var appearanceJson = new JObject(
-                        new JProperty("start", appearance.Start),
-                        new JProperty("end", appearance.End));
-
-                    appearances.Add(appearanceJson);
-                }
-
-                var newEntry = new JObject(
-                    new JProperty("type", "image"),
-                    new JProperty("text", entry.Text),
-                    new JProperty("image_url", entry.ImageUrl),
-                    new JProperty("appears", appearances));
-
-                entriesJson.Add(newEntry);
-            }
-
-            JObject timelineCard = new JObject(
-                            new JProperty("type", "skill_card"),
-                            new JProperty("skill_card_type", "timeline"),
-                            new JProperty("title", title),
-                            new JProperty("skill",
-                                new JObject(
-                                    new JProperty("type", "service"),
-                                    new JProperty("id", serviceName))),
-                            new JProperty("invocation",
-                                new JObject(
-                                    new JProperty("type", "skill_invocation"),
-                                    new JProperty("id", skillInvocationId))),
-                            new JProperty("entries", entriesJson));
-
-            if (duration.HasValue)
-            {
-                timelineCard.Add("duration", duration.Value);
-            }
-
-            return timelineCard;
-        }
-
-        public struct TranscriptCardEntry
-        {
-            public string Text { get; }
-            public IList<SkillsCardEntryAppearance> Appearances { get; }
-
-            public TranscriptCardEntry(string text, IList<SkillsCardEntryAppearance> appearances)
-            {
-                Text = text;
-                Appearances = appearances;
-            }
-        }
-
-        public struct KeywordCardEntry
-        {
-            public string Text { get; }
-            public IList<SkillsCardEntryAppearance> Appearances { get; }
-
-            public KeywordCardEntry(string text, IList<SkillsCardEntryAppearance> appearances)
-            {
-                Text = text;
-                Appearances = appearances;
-            }
-        }
-
-        public struct TimelineCardEntry
-        {
-            public string Text { get; }
-            public string ImageUrl { get; }
-            public IList<SkillsCardEntryAppearance> Appearances { get; }
-
-            public TimelineCardEntry(string text, string imageUrl, IList<SkillsCardEntryAppearance> appearances)
-            {
-                Text = text;
-                ImageUrl = imageUrl;
-                Appearances = appearances;
-            }
-        }
-
-        public struct SkillsCardEntryAppearance
-        {
-            public double Start { get; }
-            public double End { get; }
-
-            public SkillsCardEntryAppearance(double start, double end)
-            {
-                Start = start;
-                End = end;
-            }
         }
     }
 }
